c4c40ee4a5ca5e712d30538d46cf26bb6ca5c06b
Superwhiskers <kspy360@rocketmail.com>
2018-04-14 12:26:10 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+/*
+
+custom-rpc/index.js
+
+the only nodejs file in this project
+
+written by superwhiskers, licensed under gnu agpl.
+if you want a copy, go to http://www.gnu.org/licenses/
+
+*/
+
+// get some modules
+const yaml = require("js-yaml"),
+      rpc = require("discord-rich-presence"),
+      fs = require("fs"),
+      ps = require("ps-node")
+
+// config placeholder variable
+var cfg
+
+// read the config file
+try {
+
+    // parse and load the config file
+    cfg = yaml.safeLoad(
+        fs.readFileSync(
+            "config.yaml",
+            "utf8"
+        )
+    )
+
+} catch (err) {
+
+    // show the error
+    console.log("[err]: error while loading config.yaml. check if it exists.")
+    console.log("       if it does, then check the validity of the contents")
+    console.log(`       error: ${ err }`)
+
+    // then exit
+    process.exit()
+
+}
+
+// now, if we successfully loaded it, then get the key from it
+var client = rpc(cfg["key"])
+
+// show the user that we have started
+console.log("it should be working now ~~")
+
+// and set an interval to update it
+setInterval(() => {
+
+    client.updatePresence(cfg["presence"])
+
+}, 15e3)
---
+++ readme.md
@@ -0,0 +1,15 @@
+# custom-rpc
+
+## superwhisker's custom rich presence for discord
+
+### why is this even online
+
+because, i'm committed to making any code that i write publicly available, whether you like it or not
+
+### how do i use this
+
+- install [nodejs](https://www.nodejs.org)
+- clone this repository
+- run `npm i` in the project directory
+- copy `config.example.yaml` to a file named `config.yaml` and edit the contents to your liking
+- run `node index.js`
